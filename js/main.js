8fc5538fe29af3b8bb038964ab8af168a229d294
Derek Daisey <dmdaisey@yahoo.com>
2019-03-14 23:18:35 -0400

    Finishing touches. Beautified.

--- js/main.js
+++ js/main.js
@@ -1,26 +1,17 @@
-// $('.jpg').lightbox({
-//   maxHeight: 500,
-//   maxWidth: 500
-// })
+searchBar.addEventListener("keyup", function() {
+  // Declare variables
+  const input = document.getElementById('searchBar');
+  const filter = input.value.toUpperCase();
+  const ul = document.getElementById('myImages');
+  const li = ul.getElementsByTagName('a');
 
-// $(document).ready(function() {
-// 	$('#search').hideseek();
-// });
-
-searchBar.addEventListener( "keyup", function() {
-// Declare variables
-const input = document.getElementById('searchBar');
-const filter = input.value.toUpperCase();
-const ul = document.getElementById('myImages');
-const li = ul.getElementsByTagName('a');
-
-//Loop through all list items, and hide those who don't match the search query
-for (i = 0; i < li.length; i += 1) {
-  photoInfo = li[i].getAttribute("data-title");
-  if (photoInfo.toUpperCase().indexOf(filter) > -1) {
-    li[i].style.display = "";
-  } else {
-    li[i].style.display = "none";
+  //Loop through all list items, and hide those who don't match the search query
+  for (i = 0; i < li.length; i += 1) {
+    photoInfo = li[i].getAttribute("data-title");
+    if (photoInfo.toUpperCase().indexOf(filter) > -1) {
+      li[i].style.display = "";
+    } else {
+      li[i].style.display = "none";
+    }
   }
-}
 });
