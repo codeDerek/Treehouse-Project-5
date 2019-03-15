abb86c499afa613f93072097559a0412ce8c4a1e
Derek Daisey <dmdaisey@yahoo.com>
2019-03-14 21:05:34 -0400

    more js tinkering

--- js/main.js
+++ js/main.js
@@ -9,17 +9,15 @@
 
 searchBar.addEventListener( "keyup", function() {
 // Declare variables
-var input, filter, ul, li, a, i, txtValue;
-input = document.getElementById('searchBar');
-filter = input.value.toLowerCase();
-ul = document.getElementById('myImages');
-li = ul.getElementsByTagName('li');
+const input = document.getElementById('searchBar');
+const filter = input.value.toUpperCase();
+const ul = document.getElementById('myImages');
+const li = ul.getElementsByTagName('li');
 
 //Loop through all list items, and hide those who don't match the search query
 for (i = 0; i < li.length; i += 1) {
   photoInfo = li[i].getAttribute("data-title");
-  txtValue = photoInfo.textContent || photoInfo.innerText;
-  if (txtValue.toLowerCase().indexOf(filter) > -1) {
+  if (photoInfo.toUpperCase().indexOf(filter) > -1) {
     li[i].style.display = "";
   } else {
     li[i].style.display = "none";
